69469e14ce3164998b7bac636358f4d378b05838
Faisal Khan <faisaldev404@gmail.com>
2023-11-11 19:16:19 +0500

    connecting mongodb

--- server/index.js
+++ server/index.js
@@ -1,6 +1,23 @@
 import express from "express";
+import dotenv from 'dotenv';
+import mongoose from "mongoose";
 
-const app = express()
+
+
+
+const app = express();
+
+dotenv.config();
+
+//connect mongodb. 
+
+mongoose.connect(process.env.MONGO_CONNECTION).then(() => {
+    console.log('the Mongodb is connected')
+})
+    .catch((err) => {
+        console.log('error ', err)
+    })
+
 
 
 app.listen(3000, () => [
