14398ed6cebe96b3ffa28acdca8f9ef60d2a5e93
Faisal Khan <faisaldev404@gmail.com>
2023-11-15 12:51:46 +0500

    feat: update profile image. check!

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,19 +1,17 @@
-import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import { useRef } from 'react';
 const Profile = () => {
-    const [formData, setFormData] = useState({})
-    const { error, loading, currentUser } = useSelector((state) => state.user)
-    function handleChange(e) {
-        setFormData({
-            ...formData,
-            [e.target.id]: e.target.value,
-        });
-    }
+    const { loading, currentUser } = useSelector((state) => state.user)
+    const fileRef = useRef()
     return (
         <div className='flex flex-col max-w-lg mx-auto'>
-            <div className='text-center mt-9 flex justify-center items-center gap-2'>HEY <strong className='text-green-500'>{currentUser.username.toUpperCase()} 😊</strong>
+            <div className='text-center mt-9 flex justify-center items-center gap-2'>HEY <strong className='text-green-500'>{currentUser.username.toUpperCase()} </strong>
             </div>
-            <img src={currentUser.profilePicture} className='rounded-full object-cover  h-13 w-16 block m-auto mt-2 cursor-pointer' />
+            <input type="file" ref={fileRef} hidden />
+            <img
+                src={currentUser.profilePicture}
+                className='rounded-full object-cover  h-13 w-16 block m-auto mt-2 cursor-pointer'
+                onClick={() => fileRef.current.click()} />
             <p className='text-center'> You create this account at
                 <strong > {new Date(currentUser.createdAt).toLocaleDateString()}</strong>
             </p>
@@ -25,7 +23,6 @@ const Profile = () => {
                     id="username"
                     placeholder="Your username"
                     className="bg-slate-100 p-3 rounded-lg"
-                    onChange={handleChange}
                     defaultValue={currentUser.username}
                 />
                 <input
@@ -33,7 +30,6 @@ const Profile = () => {
                     id="email"
                     placeholder="Enter Your Email"
                     className="bg-slate-100 p-3 rounded-lg"
-                    onChange={handleChange}
                     defaultValue={currentUser.email}
                 />
                 <input
@@ -41,21 +37,19 @@ const Profile = () => {
                     id="password"
                     placeholder="Change your password"
                     className="bg-slate-100 p-3 rounded-lg"
-                    onChange={handleChange}
-
                 />
                 <button
                     disabled={loading}
                     type="submit"
                     className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-75 "
                 > Update </button>
-                <div className='flex justify-between  text-red-600 '>
-                    <span>Delete Account</span>
-                    <span>Logout</span>
-
-                </div>
 
             </form>
+            <div className='flex justify-between  text-red-600 '>
+                <span>Delete Account</span>
+                <span>Logout</span>
+
+            </div>
 
 
         </div>
